857ebef3339939e93425cf1a1347058c57c0817d
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2018-11-30 02:18:03 +0700

    code PeopleDTO

---
+++ 04_Implement/02_Source/DTO/PeopleDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class PeopleDTO
+    {
+        private string id;
+        private string password;
+        private string type;
+        private string name;
+        private string dateofBith;
+        private string gender;
+        private string email;
+        private string phone;
+
+        public string Id { get => id; set => id = value; }
+        public string Password { get => password; set => password = value; }
+        public string Type { get => type; set => type = value; }
+        public string Name { get => name; set => name = value; }
+        public string DateofBith { get => dateofBith; set => dateofBith = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string Email { get => email; set => email = value; }
+        public string Phone { get => phone; set => phone = value; }
+
+        public PeopleDTO()
+        {
+            Id = Password = Type = Name = DateofBith = Gender = Email = Phone = "";
+        }
+    }
+}
