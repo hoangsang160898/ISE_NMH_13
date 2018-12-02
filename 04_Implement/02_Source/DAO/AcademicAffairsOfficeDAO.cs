c44c220cc6c2875e12d0ba0830716132f2a8a25b
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2018-12-03 02:23:07 +0700

    update DAO

---
+++ 04_Implement/02_Source/DAO/AcademicAffairsOfficeDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+namespace DAO
+{
+    class AcademicAffairsOfficeDAO
+    {
+        // load danh sách sinh viên theo lớp, theo năm học
+        static SqlConnection con;
+        public static List<StudentDTO> LoadStudent(string className, string SchoolYear)
+        {
+            string sTruyVan = @"Select IDStudent, Name, Gender,Email, Phone, BirthDay from Student where nameClass = " + className + @" and ClassSchoolYear = " + SchoolYear;
+            con = DataProvider.OpenConnection();
+            DataTable dt = DataProvider.GetDataTable(sTruyVan, con);
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            List<StudentDTO> result = new List<StudentDTO>();
+            for (int i=0;i<dt.Rows.Count;i++)
+            {
+                StudentDTO student = new StudentDTO();
+                student.Id = dt.Rows[i]["IDStudent"].ToString();
+                student.Name = dt.Rows[i]["Name"].ToString();
+                student.Gender = dt.Rows[i]["Gender"].ToString();
+                student.Email = dt.Rows[i]["Email"].ToString();
+                student.Phone = dt.Rows[i]["Phone"].ToString();
+                student.DateofBith = dt.Rows[i]["BirthDay"].ToString();
+                result.Add(student);
+            }
+            return result;
+        }
+    }
+}
