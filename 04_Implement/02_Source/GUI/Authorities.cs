3fbd9a9abb58946be1149e1bc32a8f8c1bb30fbf
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2018-12-03 16:27:58 +0700

    update GUI

---
+++ 04_Implement/02_Source/GUI/Authorities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class Authorities
+    {
+        public int AuthorID { get; set; } // 3 quyền đăng nhập admin 1, student -1, teacher 1
+        public string AuthorName { get; set; } 
+        public string AuthorIcon { get; set; }
+    }
+    public class AuthoritiesManager
+    {
+        public static List<Authorities> GetAuthorities()
+        {
+            var list_Authorities = new List<Authorities>();
+            list_Authorities.Add(new Authorities { AuthorID = 1, AuthorName="Teacher", AuthorIcon="img/icons/teacher_icon_white.png" });
+            list_Authorities.Add(new Authorities { AuthorID = 0, AuthorName = "Student", AuthorIcon = "img/icons/student_icon_white.png" });
+            list_Authorities.Add(new Authorities { AuthorID = -1, AuthorName = "Admin", AuthorIcon = "img/icons/admin_icon_white.png" });
+            return list_Authorities;
+        }
+    }
+}
---
+++ 04_Implement/02_Source/GUI/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string userAdmin = "admin";
+        string passAdmin = "123";
+        string userLogIn, passLogIn = "";
+        private DispatcherTimer dispatcherTimer;
+        private DispatcherTimer dispatcherTimerForTip;
+        private List<Authorities> list_authorities;
+        public MainWindow()
+        {
+            //Thời gian báo lỗi khi nhập user hoặc pass
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+
+            //Thời gian của notification hello
+            dispatcherTimerForTip = new DispatcherTimer();
+            dispatcherTimerForTip.Tick += new EventHandler(dispatcherTimerForTip_Tick);
+            dispatcherTimerForTip.Interval = new TimeSpan(0, 0, 4);
+            dispatcherTimerForTip.Start();
+
+            list_authorities = AuthoritiesManager.GetAuthorities();
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            userLogIn = txtbUsername.Text;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnMini_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            passLogIn = passbPassword.Password;
+            if (userLogIn == null || userLogIn != userAdmin)
+            {
+                lblErrorMessage_1.Content = "The username that you've entered" + "\n" + "doesn't match any account.";
+                lblErrorMessage_1.Visibility = Visibility.Visible;
+                uiErrorSp_1.Visibility = Visibility.Visible;
+
+                dispatcherTimer.Start();
+            }
+            else if (passLogIn != passAdmin)
+            {
+                lblErrorMessage_2.Content = "The password that you've entered" + "\n" + "is incorrect.";
+                lblErrorMessage_2.Visibility = Visibility.Visible;
+                uiErrorSp_2.Visibility = Visibility.Visible;
+        
+                dispatcherTimer.Start();
+
+            }
+            else if (userLogIn == userAdmin & passLogIn == passAdmin)
+            {
+                this.Close();
+            }
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            lblErrorMessage_1.Visibility = Visibility.Collapsed;
+            uiErrorSp_1.Visibility = Visibility.Collapsed;
+     
+            lblErrorMessage_2.Visibility = Visibility.Collapsed;
+            uiErrorSp_2.Visibility = Visibility.Collapsed;
+          
+            dispatcherTimer.IsEnabled = false;
+        }
+        
+        private void dispatcherTimerForTip_Tick(object sender, EventArgs e)
+        {
+            lblTip.Visibility = Visibility.Collapsed;
+            uiTipSp.Visibility = Visibility.Collapsed;
+            borderTip.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var items = list_authorities;
+            test.ItemsSource = items;
+        }
+        private void test_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
