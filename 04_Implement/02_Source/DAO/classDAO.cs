9e3e4cd00f8b78fcad9d63ae58bf31ed61ee60bd
Lê Hoàng Sang <1612557@student.hcmus.edu.vn>
2018-12-04 17:20:30 +0700

    upload DAO

---
+++ 04_Implement/02_Source/DAO/classDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+namespace DAO
+{
+   public class classDAO
+    {
+        static SqlConnection con;
+        // hàm load danh sách lớp học (để đưa vào comboBox)
+        public static List<ClassDTO> loadListClass()
+        {
+            string sTruyVan = @"Select* from Class";
+            con = DataProvider.OpenConnection();
+            DataTable dt = DataProvider.GetDataTable(sTruyVan, con);
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            List<ClassDTO> result = new List<ClassDTO>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ClassDTO Class = new ClassDTO();
+                Class.Name = dt.Rows[i]["nameClass"].ToString();
+                Class.SchoolYear = dt.Rows[i]["schoolYear"].ToString();
+                Class.IdMaster = dt.Rows[i]["IDMaster"].ToString();
+                result.Add(Class);
+            }
+            DataProvider.CloseConnection(con);
+            return result;
+        }
+    }
+}
--- 04_Implement/02_Source/DTO/SubjectDTO.cs
+++ 04_Implement/02_Source/DTO/SubjectDTO.cs
@@ -10,33 +10,15 @@ namespace DTO
     {
         private string idSubject;
         private string nameSubject;
-        private double firstFifteenMinutesMark;
-        private double secondFifteenMinutesMark;
-        private double thirdFifteenMinutesMark;
-        private double firstFortyFiveMinutesMark;
-        private double secondFortyFiveMinutesMark;
-        private double thirdFortyFiveMinutesMark;
-        private double semesterScore;
-        private double averageMark;
+    
 
         public string IdSubject { get => idSubject; set => idSubject = value; }
         public string NameSubject { get => nameSubject; set => nameSubject = value; }
-        public double FirstFifteenMinutesMark { get => firstFifteenMinutesMark; set => firstFifteenMinutesMark = value; }
-        public double SecondFifteenMinutesMark { get => secondFifteenMinutesMark; set => secondFifteenMinutesMark = value; }
-        public double ThirdFifteenMinutesMark { get => thirdFifteenMinutesMark; set => thirdFifteenMinutesMark = value; }
-        public double FirstFortyFiveMinutesMark { get => firstFortyFiveMinutesMark; set => firstFortyFiveMinutesMark = value; }
-        public double SecondFortyFiveMinutesMark { get => secondFortyFiveMinutesMark; set => secondFortyFiveMinutesMark = value; }
-        public double ThirdFortyFiveMinutesMark { get => thirdFortyFiveMinutesMark; set => thirdFortyFiveMinutesMark = value; }
-        public double SemesterScore { get => semesterScore; set => semesterScore = value; }
-        public double AverageMark { get => averageMark; set => averageMark = value; }
+       
 
         public SubjectDTO()
         {
             idSubject = NameSubject = "";
-            firstFifteenMinutesMark = secondFifteenMinutesMark = thirdFifteenMinutesMark = 0;
-            FirstFortyFiveMinutesMark = SecondFortyFiveMinutesMark = ThirdFortyFiveMinutesMark = 0;
-            semesterScore = 0;
-            averageMark = 0;
         }
     }
 }
