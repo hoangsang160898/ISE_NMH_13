e9c02639b3adab20f5570f24585e5f5f8d36c370
Nhien <thuynhien030297@gmail.com>
2018-12-08 17:09:01 +0700

    upload TeacherDAO

---
+++ 04_Implement/02_Source/DAO/TeacherDAO.cs
@@ -0,0 +1,78 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class TeacherDAO
+    {
+        static SqlConnection con;
+        // Login va load thong tin
+        public static TeacherDTO Login(string id, string pw)
+        {
+            string sTruyVan = @"select * from Teacher where IDTeacher= '" + id + @"' and PassWord = '" + pw + "'";
+            con = DataProvider.OpenConnection();
+            DataTable dt = DataProvider.GetDataTable(sTruyVan, con);
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            TeacherDTO teacher = new TeacherDTO();
+
+            teacher.Id = dt.Rows[0]["IDTeacher"].ToString();
+            teacher.Name = dt.Rows[0]["Name"].ToString();
+            teacher.Gender = dt.Rows[0]["Gender"].ToString();
+            teacher.Email = dt.Rows[0]["Email"].ToString();
+            teacher.Phone = dt.Rows[0]["Phone"].ToString();
+            teacher.DateofBith = dt.Rows[0]["BirthDay"].ToString();
+            DataProvider.CloseConnection(con);
+            return teacher;
+        }
+
+        // Giao vien chu nhiem xem diem cua tat ca cac mon o lop do minh chu nhiem
+        public static DataTable LoadMarkMaster(string idteacher)
+        {
+            string sTruyVan = @"select e.NameSubject, c.Name, c.nameClass, c.schoolYear, d.Semester, d.FirstFifteenMinutes, d.SecondFifteenMinutes, d.ThirdFifteenMinutes, d.FirstFortyFiveMinutes, d.SecondFortyFiveMinutes, d.ThirdFortyFiveMinutes, d.SemesterMark, d.IDStudent, d.IDSubject
+                                from Teacher a  join Class b on a.IDTeacher= b.IDMaster 
+	                                            join Student c on c.nameClass = b.nameClass and c.schoolYear = b.schoolYear
+	                                            join Mark d on d.IDStudent= c.IDStudent and d.nameClass= c.nameClass and d.schoolYear=c.schoolYear
+	                                            join Subject e on d.IDSubject = e.IDSubject
+
+                                where a.IDTeacher= '" + idteacher + "'";
+            con = DataProvider.OpenConnection();
+            DataTable dt = DataProvider.GetDataTable(sTruyVan,con);
+            return dt;
+        }
+
+        // Giao vien bo mon xem diem cua hoc sinh do giao vien do day
+        public static DataTable LoadMark(string idteacher)
+        {
+            string sTruyVan = @"select c.NameSubject, e.Name, d.nameClass, d.schoolYear, d.Semester, d.FirstFifteenMinutes, d.SecondFifteenMinutes, d.ThirdFifteenMinutes, d.FirstFortyFiveMinutes, d.SecondFortyFiveMinutes, d.ThirdFortyFiveMinutes, d.SemesterMark, d.IDStudent, d.IDSubject
+                                        from Teacher a  join Teacher_Subject b on a.IDTeacher = b.IDTeacher
+				                                        join Subject c on c.IDSubject = b.IDSubject 
+				                                        join Mark d on d.IDSubject = b.IDSubject and d.IDTeacher = b.IDTeacher
+				                                        join Student e on d.IDStudent = e.IDStudent
+                                where a.IDTeacher=  '" + idteacher + "'";
+            con = DataProvider.OpenConnection();
+            DataTable dt = DataProvider.GetDataTable(sTruyVan, con);
+            return dt;
+
+        }
+        // giao vien thay doi diem
+        public static void changeMark(string idStudent, string idSubject, string semester, float FirstFifteenMinutes, float SecondFifteenMinutes, float ThirdFifteenMinutes, float FirstFortyFiveMinutes, float SecondFortyFiveMinutes, float ThirdFortyFiveMinutes, float SemesterMark)
+        {
+            string sTruyVan = @"update Mark 
+                                set FirstFifteenMinutes = "+ FirstFifteenMinutes + @", SecondFifteenMinutes ="+ SecondFifteenMinutes + ",ThirdFifteenMinutes ="+ ThirdFifteenMinutes + @", FirstFortyFiveMinutes="+ FirstFortyFiveMinutes + @", SecondFortyFiveMinutes="+ SecondFortyFiveMinutes + @", ThirdFortyFiveMinutes="+ ThirdFortyFiveMinutes + @", SemesterMark="+ SemesterMark + @"
+                                where IDStudent = '"+ idStudent+@"',  IDSubject ='"+ idSubject + "',Semester='"+ semester + "'";
+            con = DataProvider.OpenConnection();
+            DataProvider.ExecuteQuery(sTruyVan, con);
+            
+        }
+
+    }
+}
