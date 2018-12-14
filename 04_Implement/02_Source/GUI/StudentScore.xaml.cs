c4fae79f45f3a7603b1ef59a9b8d4ec29300878a
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2018-12-14 20:22:03 +0700

    update GUI

--- 04_Implement/02_Source/GUI/StudentScore.xaml.cs
+++ 04_Implement/02_Source/GUI/StudentScore.xaml.cs
@@ -21,11 +21,9 @@ namespace GUI
     public partial class StudentScore : Page
     {
         List<MarkDTO> marks = new List<MarkDTO>();
-        List<SubjectDTO> subjects = new List<SubjectDTO>();
-        List<ClassDTO> classes = new List<ClassDTO>();
         public StudentScore()
         {
-            marks.Add(new MarkDTO { FirstFifteenMinutesMark = 1, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "A" });
+            marks.Add(new MarkDTO { FirstFifteenMinutesMark = 1, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "Information Technology" });
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 2, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "A" });
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 3, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "B" });
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 4, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "C" });
@@ -34,18 +32,13 @@ namespace GUI
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 7, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "F" });
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 8, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "G" });
             marks.Add(new MarkDTO { FirstFifteenMinutesMark = 9, SecondFifteenMinutesMark = 5, ThirdFifteenMinutesMark = 9, FirstFortyFiveMinutesMark = 10, SecondFortyFiveMinutesMark = 3, ThirdFortyFiveMinutesMark = 8.5, SemesterScore = 9.5, IdSubject = "H" });
-            classes.Add(new ClassDTO { SchoolYear = "2010" });
-            classes.Add(new ClassDTO { SchoolYear = "2011" });
-            classes.Add(new ClassDTO { SchoolYear = "2012" });
-            classes.Add(new ClassDTO { SchoolYear = "2013" });
             InitializeComponent();
         }
 
         private void Window_Loaded_Score(object sender, RoutedEventArgs e)
         {
             var testGUI = marks;
-            test.ItemsSource = testGUI;
-           
+            test.ItemsSource = testGUI;         
         }
     }
 }
