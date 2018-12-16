b173089032ffcd37100a6e1a534b55e52cd8d47f
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2018-12-16 23:49:15 +0700

    update GUI

--- 04_Implement/02_Source/GUI/AdminManageUser.xaml.cs
+++ 04_Implement/02_Source/GUI/AdminManageUser.xaml.cs
@@ -18,12 +18,12 @@ namespace GUI
     /// <summary>
     /// Interaction logic for test2.xaml
     /// </summary>
-    public partial class AdminManageUser : Page
+    public sealed partial class AdminManageUser : Page
     {
         private List<PeopleDTO> users = new List<PeopleDTO>();
         public AdminManageUser()
         {
-            users.Add(new PeopleDTO {Id ="1612556",Name="Nguyen Hoang Sang", Type="Student", Status="Active"});
+            users.Add(new PeopleDTO {Id ="1612556",Name="Nguyen Hoang Sang", Type="Student", Status="Active",DateofBith="16/8/1998",Gender="Male",Email="test@gmail.com",Password="123",Phone="0123456789"});
             users.Add(new PeopleDTO { Id = "1612556", Name = "Nguyen Hoang Sang", Type = "Student", Status = "Active" });
             users.Add(new PeopleDTO { Id = "1612557", Name = "Le Hoang Sang", Type = "Teacher", Status = "Active" });
             users.Add(new PeopleDTO { Id = "1512383", Name = "Nguyen Thuy Nhien", Type = "Student", Status = "Active" });
@@ -38,13 +38,15 @@ namespace GUI
 
         private void Window_Loaded_User(object sender, RoutedEventArgs e)
         {
-            test.ItemsSource = users;
+            listviewUser.ItemsSource = users;
+            
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
             btnDoneOfEdit.Visibility = Visibility.Visible;
             btnDelete.Visibility = Visibility.Visible;
+            btnActive.Visibility = Visibility.Visible;
         }
 
         private void btnDoneofEdit_click(object sender, RoutedEventArgs e)
@@ -52,6 +54,7 @@ namespace GUI
             btnEdit.Visibility = Visibility.Visible;
             btnDoneOfEdit.Visibility = Visibility.Collapsed;
             btnDelete.Visibility = Visibility.Collapsed;
+            btnActive.Visibility = Visibility.Collapsed;
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
@@ -60,11 +63,40 @@ namespace GUI
             btnEdit.Visibility = Visibility.Visible;
             btnDoneOfEdit.Visibility = Visibility.Collapsed;
             btnDelete.Visibility = Visibility.Collapsed;
+            btnActive.Visibility = Visibility.Collapsed;
         }
 
         private void test_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             btnEdit.IsEnabled = true;
+        }
+
+        private void btnActive_Click(object sender, RoutedEventArgs e)
+        {
+            btnEdit.IsEnabled = false;
+            btnEdit.Visibility = Visibility.Visible;
+            btnDoneOfEdit.Visibility = Visibility.Collapsed;
+            btnDelete.Visibility = Visibility.Collapsed;
+            btnActive.Visibility = Visibility.Collapsed;
+        }
+
+        private void SelectItem(object sender, MouseButtonEventArgs e)
+        {
+            PeopleDTO item = (PeopleDTO)listviewUser.SelectedItems[0];
+            fullname_user_infor.Text = item.Name;
+            birthofday_user_infor.Text = item.DateofBith;
+            email_user_infor.Text = item.Email;
+            password_user_infor.Password = item.Password;
+            gender_user_infor.Text = item.Gender;
+            phone_user_infor.Text = item.Phone;
+            author_user_infor.Text = item.Type;
+            if (item.Status == "Active")
+            {
+                btnDelete.Visibility = Visibility.Visible;
+            }else if (item.Status == "Deactive")
+            {
+                btnActive.Visibility = Visibility.Visible;
+            }
         }
     }
 }
